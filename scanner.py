36c6834a6002f4165387d7db027e4b604d21a9ef
AlirezaDizaji <alirezadizaji@yahoo.com>
2022-04-08 16:42:54 +0430

    define abstract DFA, implement NUMBER token extraction

--- scanner.py
+++ scanner.py
@@ -1,7 +1,21 @@
+from enum import Enum
+from typing import Callable, List, Optional, Tuple
+
+from .tools.dfa import *
+
+
+class TokenType(Enum):
+    NUMBER = "NUMBER"
+    ID = "ID"
+    KEYWORD = "KEYWORD"
+    SYMBOL = "SYMBOL"
+    COMMENT = "COMMENT" # seems ugly :)
+    WHITESPACE = "WHITESPACE"
+
 class Token:
-    def __init__(self, type, lexeme):
-        self.type = type
-        self.lexeme = lexeme
+    def __init__(self, type: TokenType, lexeme: str) -> None:
+        self.type: TokenType = type
+        self.lexeme: str = lexeme
 
 class SymbolTable:
         def __init__(self):
@@ -14,42 +28,98 @@ class Error:
             self.line = line
             self.text = text
             self.type = type
-
-from ctypes import pointer
-from enum import Enum
-from typing import List
-
 class ErrorType(Enum):
     INVALID_INPUT = 1
     INVALID_NUMBER = 2
     UNCLOSED_COMMENT = 3
     UNMATCHED_COMMENT = 4
 
-
 class Scanner:
     def __init__(self, input_address: str) -> None:
         self.input_file: str = open(input_address).read()
         self.current_line_num: int = 1
+        self.current_token_type: TokenType = None
         self.pointer1: int = 0
-        self.pointer2: int = 0
+        self.pointer2: int = self.pointer1
 
         self.tokens: List[Token] = list()
         self.symbol_table = SymbolTable()
         self.errors: List[Error] = list()
 
-    def select_dfa(self):
-        pass
+    def _select_dfa(self, first_char: str) -> Optional[DFA]:
+        """specifies suitable DFA for the current token
 
-    def _get_next_token(self): # handle \n here
+        Args:
+            first_char (str): first character of the current token
+        """
+
         first_char = self.input_file[self.pointer1]
-        dfa = self.select_dfa(first_char)
-        output = dfa(self.pointer1, self.input_file) # also decides pointer2, and errors
-        return output
+        if first_char.isdigit():
+            self.current_token_type = TokenType.NUMBER
+            return NumberDFA
+        
+        return None
+
+    def _get_err_type(self, lexeme: str) -> ErrorType:
+        """specifies type of the leximal error"""
+
+        if self.current_token_type == TokenType.NUMBER:
+            return ErrorType.INVALID_NUMBER        
+        elif lexeme == "*/":
+            return ErrorType.UNMATCHED_COMMENT
+        elif self.current_token_type is None:
+            return ErrorType.INVALID_INPUT
+
+    def _get_next_token(self) -> Tuple[Optional[Token], Optional[Error]]:
+        dfa: Optional[DFA] = self._select_dfa()
+        token: Optional[Token] = None
+        err: Optional[Error] = None
+        
+        if dfa is not None:       
+            while True:
+                ch = self.input_file[self.pointer2]
+            
+                # handle new line
+                if ch=="\n":
+                    self.current_line_num += 1
+            
+                dfa.move(ch)
+                if dfa.state in [FINAL_STATE, UNKNOWN]:
+                    break
+                self.pointer2 += 1
+
+            if dfa.lookahead:
+                self.pointer2 -= 1
+
+            # process new token
+            if dfa.state == FINAL_STATE and self.current_token_type != TokenType.COMMENT:
+                lexeme = self.input_file[self.pointer1: self.pointer2 + 1]
+                token_type = self.current_token_type
+                token = Token(token_type, lexeme)
+                self.tokens.append(token)
+
+        # process error lexeme
+        if dfa is None or (dfa is not None and dfa.state == UNKNOWN):
+            if self.current_token_type != TokenType.COMMENT:
+                lexeme = self.input_file[self.pointer1: self.pointer2 + 2]
+            else:
+                # NOTE: handle error lexeme of comments (just the first 10 characters)
+                pass
+            err_type = self._get_err_type()
+            err = Error(self.current_line_num, lexeme, err_type)
+            self.errors.append(err)
+
+        # update attrs for extracting new token
+        self.pointer2 += 1
+        self.pointer1 = self.pointer2
+        dfa.reset()
+
+        return token, err
 
 
     def scan(self):                
         while self.pointer1 < len(self.input_file):
-            token, error, is_token_finished = self.get_next_token() # ISSUE: is is_token finished needed?
+            token, error, is_token_finished = self._get_next_token() # ISSUE: is is_token finished needed?
             if is_token_finished:
                 self.pointer1 = self.pointer2
 
@@ -59,4 +129,5 @@ def ID_dfa():
     pass
 def symbol_dfa():
     pass
+        
 # complete each of these functions...
---
+++ tools/dfa.py
@@ -0,0 +1,60 @@
+from abc import abstractmethod
+from typing import Dict, List, Optional
+
+FINAL_STATE = -1
+UNKNOWN = -2
+
+class DFA:
+    """a general DFA class to define its policy"""
+    
+    state: int = 0
+    """ current state of the DFA"""
+
+    lookahead: bool = False
+    """whether lookahead occured or not"""
+
+    def reset(cls):
+        """reset DFA to initial state"""
+        state: int = 0
+        
+    @abstractmethod
+    @classmethod
+    def move(cls, action: str) -> Optional[int]:
+        """move within DFA and return next state"""
+
+
+class NumberDFA(DFA):
+
+    def move(cls, action: str):
+        state = cls.state
+        next_state: int = UNKNOWN
+
+        if state == 0 and action.isdigit():
+            next_state = 1
+
+        elif state == 1:
+            if action.isdigit():
+                next_state = 1
+            elif action == ".":
+                next_state = 2
+            elif not action.isalpha() and not action in ["!", "$"]:
+                cls.lookahead = True
+                next_state = FINAL_STATE
+        
+        elif state == 2 and action.isdigit():
+            next_state = 3
+        
+        elif state == 3:
+            if action.isdigit():
+                next_state = 3
+            elif not action.isalpha() and not action in ["!", "$"]:
+                cls.lookahead = True
+                next_state = FINAL_STATE
+        
+        if next_state == UNKNOWN:
+            cls.lookahead = True
+
+        cls.state = next_state
+        return next_state
+
+                
